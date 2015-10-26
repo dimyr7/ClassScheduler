fbf04b49f32f21e05d0c54c544fb2b7472db7a76
Dima <dimyr7.puma@gmail.com>
2015-10-25 21:56:49 -0500

    added rule-of-3 and other functionality to Time class

--- SchoolParsers/Time.cpp
+++ SchoolParsers/Time.cpp
@@ -1,68 +1,124 @@
 #include "Time.hpp"
 namespace CourseInfo{
-Time::Time(){
-	this->_hour = 0;
-	this->_minute = 0;
-}
 
-Time::Time(int hour, int minute){
-	if(not Time::validHour(hour) or not Time::validMinute(minute)){
-		this->_hour = 0;
-		this->_minute = 0;
+
+	/*
+	 * ======================================================
+	 * Object Creation
+	 * ======================================================
+	 */
+	Time::Time(int hour, int minute){
+		if(not Time::validHour(hour) or not Time::validMinute(minute)){
+			this->_hour = 0;
+			this->_minute = 0;
+		}
+		else{
+			this->_hour = hour;
+			this->_minute = minute;
+		}
+
 	}
-	else{
+	Time::Time(const Time& copy){
+		this->_hour = copy._hour;
+		this->_minute = copy._minute;
+	}
+	Time::~Time(){
+		return;
+	}
+	Time& Time::operator=(const Time& copy){
+		this->_hour = copy._hour;
+		this->_minute = copy._minute;
+		return *this;
+	}
+
+	
+	/*
+	 * ======================================================
+	 * Getters and Setters
+	 * ======================================================
+	 */
+	int Time::getHour() const {
+		return _hour;
+	}
+	int Time::getMinute() const {
+		return _minute;
+	}
+	bool Time::setHour(int hour){
+		if(not Time::validHour(hour)){
+			return false;
+		}
 		this->_hour = hour;
-		this->_minute = minute;
-	}
-
-}
-
-Time::~Time(){
-	return;
-}
-
-bool Time::validHour(int hour){
-	return (hour <= 23) and (hour >= 0);
-}
-
-bool Time::validMinute(int minute){
-	return (minute <= 59) and (minute >= 0);
-}
-
-int Time::getHour(){
-	return _hour;
-}
-
-int Time::getMinute(){
-	return _minute;
-}
-
-bool Time::setHour(int hour){
-	if(not Time::validHour(hour)){
-		return false;
-	}
-	this->_hour = hour;
-	return true;
-}
-
-bool Time::setMinute(int minute){
-	if(not Time::validMinute(minute)){
-		return false;
-	}
-	this->_minute = minute;
-	return true;
-}
-
-bool Time::before(Time* before, Time* after){
-	if(before->getHour() > after->getHour()){
-		return false;
-	}
-	else if(before->getHour() < after->getHour()){
 		return true;
 	}
-	else if(before->getMinute() >= after->getMinute()){
-		return false;
+	bool Time::setMinute(int minute){
+		if(not Time::validMinute(minute)){
+			return false;
+		}
+		this->_minute = minute;
+		return true;
 	}
-	return true;
-}
+
+	/*
+	 * ======================================================
+	 * Static functions
+	 * ======================================================
+	 */
+	bool Time::before(const Time* before, const Time* after){
+		if(before->getHour() > after->getHour()){
+			return false;
+		}
+		else if(before->getHour() < after->getHour()){
+			return true;
+		}
+		else if(before->getMinute() >= after->getMinute()){
+			return false;
+		}
+		return true;
+	}
+bool Time::validHour(int hour){
+		return (hour <= 23) and (hour >= 0);
+	}
+	bool Time::validMinute(int minute){
+		return (minute <= 59) and (minute >= 0);
+	}
+	/*
+	 * ======================================================
+	 * Additional Functionality
+	 * ======================================================
+	 */
+	Time Time::operator+(const Time& second) const{
+		Time t;
+		bool minOverflow = false;
+		if(this->_minute + second._minute> 59){
+			minOverflow = true;	
+			t._minute = (this->_minute + second._minute)%60;
+		}
+		t._hour = (this->_hour + second._hour)%24;
+		if(minOverflow){
+			t._hour++;
+		}
+		return t;
+	}
+	ostream& operator<<(ostream& os, const Time& time){
+		string min;
+		string hour;
+
+		if(time._hour <10){
+			hour = "0"+to_string(time._hour);
+		}
+		else{
+			hour = to_string(time._hour);
+		}
+
+		if(time._minute < 10){
+			min = "0"+to_string(time._minute);
+		}
+		else{
+			min = to_string(time._minute);
+		}
+
+		os<<hour<<":"<<min;
+		return os;
+	}
+
 }
--- SchoolParsers/Time.hpp
+++ SchoolParsers/Time.hpp
@@ -1,36 +1,73 @@
 #ifndef TIME_H
 #define TIME_H
+#include <iostream>
+#include <string>
+using std::string;
+using std::ostream;
+using std::to_string;
 namespace CourseInfo{
-/*
- * Represents a time on 24 hour clock
- */
-class Time{
-	public:
-		/*
-		 * Default constructor will set the time to 00:00
-		 */
-		Time();
-		/*
-		 * Sets the hour and minute to the specified parameters
-		 * If either value is invalid (e.g. hour >23, minute >59 ...) the time will be 00:00
-		 */
-		Time(int hour, int minute);
-		~Time();
-		int 	getHour();
-		int 	getMinute();
+	/*
+	 * Represents a time on 24 hour clock
+	 */
+	class Time{
+		public:
+			/*
+			 * Sets the hour and minute to the specified parameters
+			 * If either value is invalid (e.g. hour >23, hour <0, minute >59, minute <0) the time will be 00:00
+			 */
+			Time(int hour=0  , int minute=0);
+			/*
+			 * Destructor for the Time object
+			 */
+			~Time();
+			/*
+			 * Copy constructor
+			 */
+			Time(const Time& copy);
+			/*
+			 * Copy Assignment Operator
+			 */
+			Time& operator=(const Time& copy);
 
-		/*
-		 * This function will return true on success
-		 * Returns false otherwise (i.e. value is invalid) and does not change the time
-		 */
-		bool 	setHour(int hour);
-		bool 	setMinute(int minute);
-		static bool validHour(int hour);
-		static bool validMinute(int minute);
-		static bool before(Time* before, Time* after);
-	private:
-		int _hour;
-		int _minute;
-};
+			/*
+			 * returns the hour of time [0, 23]
+			 */
+			int 	getHour() 	const;
+			/*
+			 * returns the minute of the time [0, 59]
+			 */
+			int		getMinute() const;
+			/*
+			 * These functions will return true on success
+			 * Returns false otherwise (i.e. value is invalid) and does not change the time
+			 */
+			bool 	setHour(int hour);
+			bool 	setMinute(int minute);
+
+			/*
+			 * Returns true if the parameter is valid
+			 */
+			static bool validHour(int hour);
+			static bool validMinute(int minute);
+
+			/*
+			 * Returns true if Time before is before Time after
+			 */
+			static bool before(const Time* before, const Time* after);
+			/*
+			 * returns a new Time object by adding the hour and minute of the two specified times
+			 * If that causes minutes to overflow, then hour will increment additionally
+			 * IF that causes hours to overflow, then they will be modded by 24
+			 */
+			Time operator+(const Time& second) const;
+			
+			/*
+			 * friend function that is used for pring Time object to stream in the form 08:23
+			 */
+			friend ostream& operator<<(ostream& os, const Time& time);
+		private:
+			int _hour;
+			int _minute;
+	};
 }
 #endif
